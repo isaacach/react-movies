21dd306c69dc975ce34387c156adebf832c86ca5
isaacach <124728947+isaacach@users.noreply.github.com>
2023-09-20 10:14:42 -0500

    first commit

---
+++ movies/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+
+export default function Navbar() {
+
+  return (
+    <nav>
+      <div className="logo"></div>
+      <div className="links">
+        <div className="link-wrapper">
+          
+        </div>
+      </div>
+    </nav>
+  )
+}
